45c5609deb98c60aebaf9c727ae3d45930b5a62e
UwahhUnidaa <68070163@kmitl.ac.th>
2025-10-06 20:40:52 +0700

    sixth commit

--- script.js
+++ script.js
@@ -1,50 +1,44 @@
-const myinput = document.getElementById("myinput")
-
-function add() {
-    myinput.value = Number(myinput.value) + 1
-}
-
-const mytext =  document.getElementById("information")
-const myaccount = document.getElementById("myaccount")
-const mycash = document.getElementById("mycash")
-const mytype = document.getElementById("type")
-const myproceed = document.getElementById("myproceed")
-let count = 0
-let accountmoney = 0
-let cashmoney = 0
-count += 1
-mytext.value += count + " Current account balance: " + accountmoney + ", Current cash balance: " + cashmoney + "\n"
+const mytext =  document.getElementById("information");
+const myaccount = document.getElementById("myaccount");
+const mycash = document.getElementById("mycash");
+const mytype = document.getElementById("type");
+const myproceed = document.getElementById("myproceed");
+let count = 0;
+let accountmoney = 0;
+let cashmoney = 0;
+count += 1;
+mytext.value += count + " Current account balance: " + accountmoney + ", Current cash balance: " + cashmoney + "\n";
 
 function change() {
-    accountmoney = Number(myaccount.value)
-    cashmoney = Number(mycash.value)
-    count += 1
-    mytext.value += count + " Current account balance: " + accountmoney + ", Current cash balance: " + cashmoney + "\n"
+    accountmoney = Number(myaccount.value);
+    cashmoney = Number(mycash.value);
+    count += 1;
+    mytext.value += count + " Current account balance: " + accountmoney + ", Current cash balance: " + cashmoney + "\n";
 }
 
 function proceed() {
     if (mytype.value == "deposit") {
         if (Number(myproceed.value) <= cashmoney && Number(myproceed.value) >= 0) {
-            accountmoney += Number(myproceed.value)
-            cashmoney -= Number(myproceed.value)
-            count += 1
-            mytext.value += count + " Current account balance: " + accountmoney + ", Current cash balance: " + cashmoney + "\n"
+            accountmoney += Number(myproceed.value);
+            cashmoney -= Number(myproceed.value);
+            count += 1;
+            mytext.value += count + " Current account balance: " + accountmoney + ", Current cash balance: " + cashmoney + "\n";
         }
         else {
-            count += 1
-            mytext.value += count + " ERROR\n"
+            count += 1;
+            mytext.value += count + " ERROR\n";
         }
     }
     else {
         if (Number(myproceed.value) <= accountmoney && Number(myproceed.value) >= 0) {
-            accountmoney -= Number(myproceed.value)
-            cashmoney += Number(myproceed.value)
-            count += 1
-            mytext.value += count + " Current account balance: " + accountmoney + ", Current cash balance: " + cashmoney + "\n"
+            accountmoney -= Number(myproceed.value);
+            cashmoney += Number(myproceed.value);
+            count += 1;
+            mytext.value += count + " Current account balance: " + accountmoney + ", Current cash balance: " + cashmoney + "\n";
         }
         else {
-            count += 1
-            mytext.value += count + " ERROR\n"
+            count += 1;
+            mytext.value += count + " ERROR\n";
         }
     }
 }
